477dd22db87a9b1a2050455bb991047bfab1cf6a
yodai <hirano.yodai.hv4@is.naist.jp>
2019-12-09 19:37:47 +0900

    [Add] Acceleration & Gyro data dump script

--- dumper.py
+++ dumper.py
@@ -6,47 +6,49 @@ import pandas as pd
 
 if __name__ == "__main__":
     video_path = 'gps_dumper/video'
-    gps_path = 'gps_dumper/gps'
+    binary_path = 'gps_dumper/binary'
     video_list = os.listdir(video_path)
     video_numbers = len(video_list)
     column = ['VIDEO_NAME', 'UNIX_TIME', 'LATITUDE', 'LONGITUDE', 'TIME_SPAN']
-    video_gps_data = pd.DataFrame(columns=column)
+    video_gps_data = pd.DataFrame()
+    video_inertial_data = pd.DataFrame()
 
-    # 各ビデオに対して処理をする
+# 各ビデオに対して処理をする
     for number, video_name in enumerate(video_list):
+        name, codec = os.path.splitext(video_name)
+        if codec != '.MP4':
+            continue
         print(video_name+' dumping...')
-        subprocess.call(['bash', './gps_dumper/dump_gpx.sh', video_path+'/'+video_name, gps_path])
+        subprocess.call(
+            ['bash', './gps_dumper/dump_gpx.sh', video_path + '/' + video_name, binary_path]
+        )
 
-        points = []
-        indexes = []
-        name = video_name.split('.')[0]
-        gpx_file = open('./'+gps_path+'/'+name+'.gpx', 'r')
-        gpx = gpxpy.parse(gpx_file)
-        i = 0
-        for track in gpx.tracks:
-            for segment in track.segments:
-                for point in segment.points:
-                    indexes.append(name+'_'+str(i))
-                    # GPS data を抽出
-                    data = [video_name, point.time.timestamp(), point.latitude, point.longitude]
-                    if i == 0:
-                        data.append(0)
-                    else:
-                        data.append(point.time.timestamp()-points[0][1])
-                    points.append(data)
-                    i += 1
+        accl = pd.read_csv(binary_path + '/' + name + '-accl.csv', header=0)
+        gyro = pd.read_csv(binary_path + '/' + name + '-gyro.csv', header=0)
+        inertial = pd.concat([accl, gyro], axis=1)
+        gps = pd.read_csv(binary_path + '/' + name + '-gps.csv', header=0)
 
-        data_frame_buffer = pd.DataFrame(points, columns=column, index=indexes)
-        video_gps_data = pd.concat([video_gps_data, data_frame_buffer])
+        inertial['filename'] = name
+        gps['filename'] = name
+
+        gps = gps.rename(index=lambda s: name + '_' + str(s))
+        inertial = inertial.rename(index=lambda s: name + '_' + str(s))
+
+        os.remove(binary_path + '/' + name + '-accl.csv')
+        os.remove(binary_path + '/' + name + '-gyro.csv')
+        os.remove(binary_path + '/' + name + '-gps.csv')
+
+        video_inertial_data = pd.concat([video_inertial_data, inertial])
+        video_gps_data = pd.concat([video_gps_data, gps])
         print(video_name+' dump finished')
-        print('=======================STATUS=====================')
-        print(str(number+1/video_numbers*100)+'% of all process [{}/{}]'.format(number+1, video_numbers))
-        print('==================================================')
+        # print('=======================STATUS=====================')
+        # print(str(number+1/video_numbers*100)+'% of all process [{}/{}]'.format(number+1, video_numbers))
+        # print('=================================================')
 
-    # デー田を保存
+    # データを保存
     video_gps_data.to_pickle('./output/video_gps_data.zip')
+    video_inertial_data.to_pickle('./output/video_inertial_data.zip')
     video_gps_data.to_csv('./output/video_gps_data.csv')
-
-    subprocess.run(('sudo', 'rm', '-rf', gps_path+u'/*.bin'), check=True)
+    video_inertial_data.to_csv('./output/video_inertial_data.csv')
 
     print('all process finished')
